93a1216953d561acd4a0cf1483a927370f4268fc
rlap <rose.la.prairie@gmail.com>
2014-07-07 10:37:30 +0100

    oauth google added and tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,19 +11,14 @@ class User < ActiveRecord::Base
     if user = signed_in_user || User.find_by_email(auth.info.email)
       user.provider = auth.provider
       user.uid = auth.uid
-      user.name = auth.info.name if user.name.blank?
-      user.image = auth.info.image if user.image.blank?
       user.save
       user
     else
       where(auth.slice(:provider, :uid)).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
-        user.name = auth.info.name
         user.email = auth.info.email
-        user.image = auth.info.image
         user.password = Devise.friendly_token[0,20]
-        user.skip_confirmation! # don't require email confirmation
       end
     end
   end
